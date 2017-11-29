faea805d4f2c3e7f9b568f5fc1d3cd00a41bde2c
Paul J. McMurdie <joey711@gmail.com>
2017-11-28 17:41:12 -0800

    fix csc accessor tests

--- tests/testthat/test-v2_1.R
+++ tests/testthat/test-v2_1.R
@@ -9,20 +9,38 @@ packageVersion("biomformat")
 mat1 = matrix(c(1,0,2,0,0,3,4,5,6), nrow = 3, byrow = TRUE)
 rownames(mat1) <- paste0("Taxa_", 1:nrow(mat1))
 colnames(mat1) <- paste0("Sample_", 1:ncol(mat1))
-
 mat2 = as(mat1, "CsparseMatrix")
 
-
 test_that("Test biom-format v2.1 sparse CSC accessors", {
-  ### Tests
+  # index pointer
   # 0 2 3 6
-  sparse_indptr(mat1)
-  sparse_indptr(mat2)
+  expect_equal(
+    c(0, 2, 3, 6),
+    sparse_indptr(mat1)
+  )
+  expect_equal(
+    c(0, 2, 3, 6),
+    sparse_indptr(mat2)
+  )
+  # Indices
   # 0 2 2 0 1 2
-  sparse_indices(mat1)
-  sparse_indices(mat2)
+  expect_equal(
+    c(0, 2, 2, 0, 1, 2),
+    sparse_indices(mat1)
+  )
+  expect_equal(
+    c(0, 2, 2, 0, 1, 2),
+    sparse_indices(mat2)
+  )
+  # data
   # 1 4 5 2 3 6
-  sparse_data(mat1)
-  sparse_data(mat2)
+  expect_equal(
+    c(1, 4, 5, 2, 3, 6),
+    sparse_data(mat1)
+  )
+  expect_equal(
+    c(1, 4, 5, 2, 3, 6),
+    sparse_data(mat2)
+  )
 })
 
